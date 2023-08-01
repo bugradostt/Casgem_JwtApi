17d85a4566c1bfce848e9b9fe56296461f2fb1a0
bugradostt <bugradostt@gmail.com>
2023-08-01 20:13:38 +0300

    first commit

---
+++ Casgem_JwtApi/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem_JwtApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_JwtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuildToken().CreateToken());
+        }
+
+        [HttpGet("[Action]")]
+        [Authorize]
+        public IActionResult ListCustomer()
+        {
+            return Ok("Müşteri listesine eriştiniz.");
+        }
+    }
+}
